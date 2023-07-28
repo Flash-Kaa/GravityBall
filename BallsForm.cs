1da382f5dfd95990070c1c757263e6a401ac3b86
Flash-Kaa <78853080+Flash-Kaa@users.noreply.github.com>
2023-07-28 13:37:48 +0500

    Добавьте файлы проекта.

---
+++ BallsForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+	public class BallsForm : Form
+	{
+        private Timer timer;
+		private WorldModel world;
+
+        private WorldModel CreateWorldModel()
+		{
+			var w = new WorldModel
+			{
+				WorldHeight = ClientSize.Height,
+				WorldWidth = ClientSize.Width,
+				BallRadius = 10
+			};
+			w.BallX = w.WorldHeight / 2;
+			w.BallY = w.BallRadius;
+			return w;
+		}
+
+		protected override void OnResize(EventArgs e)
+		{
+			base.OnResize(e);
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			DoubleBuffered = true;
+			BackColor = Color.Black;
+			world = CreateWorldModel();
+			timer = new Timer { Interval = 30 };
+			timer.Tick += TimerOnTick;
+			timer.Start();
+			world.WorldHeight = ClientSize.Height;
+			world.WorldWidth = ClientSize.Width;
+		}
+
+		private void TimerOnTick(object sender, EventArgs eventArgs)
+		{
+			world.SimulateTimeframe(timer.Interval / 1000d);
+			Invalidate();
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+            DrawCounterAndGates(e, Color.Red, WorldModel.indent,WorldModel.CountRedGoal);
+            DrawCounterAndGates(e, Color.Green, (int)(ClientSize.Width * (1 - WorldModel.partOccupByGate)) - WorldModel.indent, WorldModel.CountGreenGoal);
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+            e.Graphics.FillEllipse(Brushes.GreenYellow, (float)(world.BallX - world.BallRadius), 
+				(float)(world.BallY - world.BallRadius), 
+				2 * (float)world.BallRadius, 2 * (float)world.BallRadius);
+        }
+
+		protected override void OnMouseMove(MouseEventArgs e)
+		{
+			base.OnMouseMove(e);
+			Text = string.Format("Cursor ({0}, {1})", e.X, e.Y);
+		}
+
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BallsForm));
+            this.SuspendLayout();
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "BallsForm";
+            this.ResumeLayout(false);
+
+		}
+        private void DrawCounterAndGates(PaintEventArgs e, Color colorRect, int startX, int goal)
+        {
+			Goalsa(e, colorRect, startX, goal);
+            Rectangle rectangle = new Rectangle(startX, WorldModel.indent, 
+				(int)(ClientSize.Width * WorldModel.partOccupByGate), ClientSize.Height - 5* WorldModel.indent);
+            e.Graphics.DrawRectangle(new Pen(colorRect), rectangle);
+        }
+
+		private void Goalsa(PaintEventArgs e, Color colorRect, int startX, int goal)
+		{ 
+			Rectangle rectangle = new Rectangle(startX, WorldModel.indent, (int)(ClientSize.Width * WorldModel.partOccupByGate), goal* (Height / WorldModel.countToWinGoals));
+            e.Graphics.FillRectangle(new SolidBrush(colorRect), rectangle);
+        }
+    }
+}
---
+++ WorldModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GravityBalls
+{
+    public class WorldModel
+    {
+        const double Gravity = 9.8;
+        const double ResisCoefForDistance = 10;
+        public double BallRadius;
+        public double BallX, BallY;
+        public double WorldWidth, WorldHeight;
+
+        public static int CountGreenGoal = 0;
+        public static int CountRedGoal = 0;
+        public static int countToWinGoals = 5;
+        public static int indent = 1;
+        public static double partOccupByGate = 0.1;
+
+        static bool BallInField = false;
+
+        double FallSpeed = 100, HorizSpeed = 100;
+        double airResist = 0.1;
+
+        public void SimulateTimeframe(double dt)
+        {
+            //меняем направление, если шар задел стену
+            FallSpeed = Reverse(FallSpeed, BallY, BallRadius, WorldHeight);
+            HorizSpeed = Reverse(HorizSpeed, BallX, BallRadius, WorldWidth);
+
+            //определяем сопротивление, противоположное направлению + силу тяжести
+            FallSpeed = DefineResistance(FallSpeed, airResist,BallX, BallY, Gravity);
+            HorizSpeed = DefineResistance(HorizSpeed, airResist, BallX, BallY, 0);
+
+            var mouse = Control.MousePosition;
+            if (BallY >= (mouse.Y - BallRadius) - 5 && BallY <= (mouse.Y + BallRadius) + 5 &&
+              BallX >= (mouse.X - BallRadius) - 5 && BallX <= (mouse.X + BallRadius) + 5)
+            {
+                if (BallY >= (mouse.Y - BallRadius) - 5 && BallY <= (mouse.Y + BallRadius) + 5)
+                    FallSpeed = Math.Sin(FallSpeed);
+                else if (BallX >= (mouse.X - BallRadius) - 5 && BallX <= (mouse.X + BallRadius) + 5)
+                    HorizSpeed = Math.Cos(HorizSpeed);
+            }
+
+            //минимум для правой и нижней стен. макс - для верхней и левой
+            BallY = PushOffTheWall(FallSpeed, BallY, BallRadius, WorldHeight, dt);
+            BallX = PushOffTheWall(HorizSpeed, BallX, BallRadius, WorldWidth, dt);
+
+            //Высчитывает очки при попадание мяча в область прямоугольников
+            CountRedGoal = GoalCount(BallX <= WorldWidth * (partOccupByGate), CountRedGoal, BallX,  WorldWidth);
+            CountGreenGoal = GoalCount(BallX >= WorldWidth * (1 - partOccupByGate), CountGreenGoal, BallX, WorldWidth);
+        }
+
+        static int GoalCount(bool crossedGate, int goals, double BallX, double WorldWidth)
+        {
+            if (CountRedGoal < countToWinGoals && !BallInField && crossedGate)
+            {
+                goals++;
+                BallInField = true;
+            }
+
+            if (BallX > WorldWidth * (partOccupByGate) && BallX < WorldWidth * (1 - partOccupByGate))
+            {
+                BallInField = false;
+            }
+
+            return goals;
+        }
+
+       static double Reverse(double speed, double Ball, double BallRadius, double worldLen)
+        {
+            if (Ball == worldLen - BallRadius || Ball == BallRadius)
+                speed = -speed;
+            return speed;
+        }
+
+        static double PushOffTheWall(double speed, double Ball, double BallRadius, double worldCoord, double time)
+        {
+            return Math.Max(Math.Min(Ball + speed * time, worldCoord - BallRadius), BallRadius);
+        }
+
+        static double DefineResistance(double speed, double airResist, double BallX, double BallY, double Gravity)
+        {
+            var mouse = Control.MousePosition;
+            double dx = mouse.X - BallX, dy = mouse.Y - BallY;
+            var distance = Math.Sqrt(dx * dx + dy * dy);
+            return speed >= 0 ? speed - airResist + Gravity - ResisCoefForDistance / distance : speed + airResist + Gravity + ResisCoefForDistance / distance;
+        }
+    }
+}
